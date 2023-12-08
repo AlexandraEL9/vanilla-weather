ec1763d57ba5ebd7ecd158f4d540a30ff1acad4d
AlexandraEL9 <alexandraelee9@gmail.com>
2023-12-08 16:14:26 +0000

    update JS and HTML

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 function refreshWeather(response) {
-  let temperatureElement = document.querySelector("temperature");
+  let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
-  let cityElement = document.querySelector("city");
+  let cityElement = document.querySelector("#city");
 
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
 }
+
 function searchCity(city) {
-  let apiKey = "06a59897dof9572ebabb740ftb4354ba";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}key=${apiKey}&units=metric`;
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(refreshWeather);
 }
 
@@ -18,6 +19,8 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
-searchCity("London");
+
+searchCity("Paris");
